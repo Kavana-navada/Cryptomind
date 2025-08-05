e169511ef74be0ee9cac7276afe9aac995d40067
kavana <kavananavada03@gmail.com>
2025-08-05 20:42:35 +0530

    designed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
-import './App.css'
+import "./App.css";
+import { Navbar } from "./components";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+
+        <div className="main"></div>
+        <div className="footer"></div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
