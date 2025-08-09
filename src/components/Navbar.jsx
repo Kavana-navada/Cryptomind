8f1f89e98efc699547248e5749261f1417c8bc30
kavana <kavananavada03@gmail.com>
2025-08-10 00:09:37 +0530

    made navbar as repsonsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import {
@@ -10,6 +10,21 @@ import {
 } from "@ant-design/icons";
 import icon from "../images/cryptocurrancy.png";
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  });
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -18,7 +33,11 @@ const Navbar = () => {
           <Link to="/">Cryptomind</Link>
         </Typography.Title>
       </div>
-      <Menu theme="dark">
+      <Button className="menu-control-container" onClick={()=>setActiveMenu((prev)=>!prev)}>
+        <MenuOutlined />
+      </Button>
+      {activeMenu && (
+        <Menu theme="dark">
         <Menu.Item icon={<HomeOutlined />}>
           <Link to="/">Home</Link>
         </Menu.Item>
@@ -35,6 +54,10 @@ const Navbar = () => {
           <Link to="/news">News</Link>
         </Menu.Item>
       </Menu>
+
+      )}
+
+      
     </div>
   );
 };
